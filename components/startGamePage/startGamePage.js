95c5374bbb6ee3f7209e9e6a41ba1e68abcb3ba9
oliviagalazka <olivia.galazka02@gmail.com>
2025-05-14 15:15:00 +0200

    resultpopup

--- components/startGamePage/startGamePage.js
+++ components/startGamePage/startGamePage.js
@@ -9,7 +9,7 @@ function renderStartGamePage(parentSelector = 'body') {
                 <h1>BLÅ</h1>
             </div>
             <div>
-                <p>Idag kommer ditt fokus, tålamod och skärpa sättas på prov. Du står på samhällets frontlinje - redo att agera när det gäller liv och säkerhet. Spelar ni flera? Samarbeta nära och spela på en gemensam enhet. Spelar du själv? Då gäller full disciplin - du har bara dig själv.</p>
+                <p>Idag kommer ditt fokus, tålamod och skärpa sättas på prov. Du står på samhällets frontlinje - redo att agera när det gäller liv och säkerhet. Spelar ni flera? Samarbeta nära och spela på varsin enhet. Spelar du själv? Då gäller full disciplin - du har bara dig själv.</p>
                 <p>Lyssna på radion. Följ instruktionerna. Lös uppdragen.</p>
                 <p>Lycka till. Och glöm inte… var inte rädda, men var rädda om er.</p>
             </div>
