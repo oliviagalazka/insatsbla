c301ba01e6823e114ed36cd1bc91e9ecaf389c85
oliviagalazka <olivia.galazka02@gmail.com>
2025-05-19 00:44:05 +0200

    css

--- data.js
+++ data.js
@@ -127,31 +127,31 @@ const reportContent = [
 let newsText = [
   {
     missionId: 1,
-    notis1: "Farliga bullar i Kungsparken",
-    notis2: "Polisens duktiga arbete i parken",
+    notis1: 'Trycket på polisen ökar - "Vi hinner inte andas"',
+    notis2: "Polisinsats stormade till Kungsparken för att hitta hundbullar",
     imgSource: "./media/pictures/hundbullar1.avif",
-    text: '<b>Bamse skadad av preparerad bulle - nya fynd av "hundbullar" i Malmö</b><br><br>Labradoren Bamse brukar springa lyckligt genom Kungsparken varje morgon, nosa på buskar och jaga löv. Men imorse tog promenaden en oväntad och smärtsam vändning. Vid dammen hann han bara nosa ett ögonblick innan han slukade en bulle någon slängt, en bulle fylld med vassa metallbitar. "Det gick så snabbt. Jag såg inte ens vad det var förrän det var för sent.", säger hans matte Ella, som snabbt fick bära honom till bilen och köra till djursjukhuset. Bamse överlevde, men skadades allvarligt internt. Veterinärer lyckades rädda honom - den här gången...<br><br>Det här är tyvärr inte ett enskilt fall. Sedan 2020 har Malmö drabbats av över 180 anmälningar om så kallade "hundbullar", enligt polisens presstalesperson Nils Norling. – Det är fruktansvärt. Man vågar knappt gå ut längre. "Det känns som ett hot mot min bästa vän, säger Ella. Polisen arbetar aktivt för att spåra gärningspersoner och stoppa de medvetna försök som görs att skada djur. Hundägare uppmanas nu till extra försiktighet. För många som Ella har vardagens enkla glädjestunder, en promenad med sin hund, förvandlats till något fyllt av oro.'
+    text: '<b>Bamse skadad av preparerad bulle - nya fynd av "hundbullar" i Malmö</b><br><br>Labradoren Bamse brukar springa lyckligt genom Kungsparken varje morgon, nosa på buskar och jaga löv. Men imorse tog promenaden en oväntad och smärtsam vändning. Vid dammen hann han bara nosa ett ögonblick innan han slukade en bulle någon slängt, en bulle fylld med vassa metallbitar. - Det gick så snabbt. Jag såg inte ens vad det var förrän det var för sent, säger hans matte Ella, som snabbt fick bära honom till bilen och köra till djursjukhuset. Bamse överlevde, men skadades allvarligt internt. Veterinärer lyckades rädda honom - den här gången...<br><br>Det här är tyvärr inte ett enskilt fall. Sedan 2020 har Malmö drabbats av över 180 anmälningar om så kallade "hundbullar", enligt polisens presstalesperson Nils Norling.<br><br>– Det är fruktansvärt. Man vågar knappt gå ut längre. Det känns som ett hot mot min bästa vän, säger Ella. Polisen arbetar aktivt för att spåra gärningspersoner och stoppa de medvetna försök som görs för att skada djur. Hundägare uppmanas nu till extra försiktighet. För många som Ella har vardagens enkla glädjestunder, en promenad med sin hund, förvandlats till något fyllt av oro.'
   },
   {
     missionId: 2,
-    notis1: "Cykeltjuv i farten",
-    notis2: "Cykelstölder i Malmö ökar lavinartat",
+    notis1: "Cykeltjuv i farten - fångad på video",
+    notis2: 'Polisen: "Vi hinner inte med alla cykelstölder"',
     imgSource: "./media/pictures/cykeltjuven.png",
-    text: "<b>Cykelsäsongen börjar dra igång på allvar - och med den kommer cykelstölderna. Från april och framåt ökar antalet anmälda cykelstölder månad för månad</b><br><br> Inte minst märks det i Malmö. 2024 gjordes 4 712 anmälningar om cykelstölder i kommunen, enligt siffror från Brottsförebyggande rådet, vilket motsvarar 13 anmälningar per 1 000 invånare. Det är tredje flest i landet och näst flest i Skåne, efter Lund - där det noterades 15,6 anmälningar per 1 000 invånare. <br><br> Dock är inte alla delar av Malmö lika hårt drabbade. Det visar data för de senaste fem åren. Det rör sig om mellan en handfull och drygt 800 anmälda stölder per område. Området med flest anmälda cykelstölder i Malmö kommun är Västra hamnen, det det handlar om 826 anmälningar under perioden 2020-2024."
+    text: "<b>Cykelsäsongen börjar dra igång på allvar - och med den kommer cykelstölderna. Från april och framåt ökar antalet anmälda cykelstölder månad för månad</b><br><br> Inte minst märks det i Malmö. 2024 gjordes 4 712 anmälningar om cykelstölder i kommunen, enligt siffror från Brottsförebyggande rådet, vilket motsvarar 13 anmälningar per 1 000 invånare. Det är tredje flest i landet och näst flest i Skåne, efter Lund - där det noterades 15,6 anmälningar per 1 000 invånare. <br><br> Dock är inte alla delar av Malmö lika hårt drabbade. Det visar data för de senaste fem åren. Det rör sig om mellan en handfull och drygt 800 anmälda stölder per område. Området med flest anmälda cykelstölder i Malmö kommun är Västra hamnen, där det handlar om 826 anmälningar under perioden 2020-2024."
   },
   {
     missionId: 3,
     notis1: "Malmö, andra staden med högst hemlöshet",
-    notis2: "Polisens arbete för hemlösa tar aldrig slut",
+    notis2: "Psykologstödet räcker inte till - fler poliser söker hjälp privat",
     imgSource: "./media/pictures/hemlose_pia.jpeg",
-    text: "<b>För tre år sedan förlorade Pia, 47 år, vårdnaden om sina barn. Hon fyllde saknaden av sina två söner med alkohol</b><br><br>Det tog inte lång tid innan hon blev avskedad från sitt kontorsjobb och drunknade i skulder hos kronofogden. Hon är numera hemlös och försöker överleva på Malmös gator. <br><br>Pia pratar sällan om sitt förflutna, men hon bär det med sig hela tiden. Bilderna på barnen har hon kvar i en sliten plastficka i jackfickan. ´Jag vet att jag gjorde misstag´, säger hon, ´men jag är fortfarande en människa. Och jag är fortfarande deras mamma.´<br><br>Trots allt håller hon fast vid en gnista hopp. Hon har börjat gå till ett stödcenter i Sankt Knut några gånger i veckan, där hon får varm mat, kläder och möjlighet att prata med en kurator. ´Det är inte mycket, men det är något. Och ibland är något tillräckligt för att orka en dag till.´"
+    text: '<b>För tre år sedan förlorade Pia, 47 år, vårdnaden om sina barn. Hon fyllde saknaden av sina två söner med alkohol</b><br><br>Det tog inte lång tid innan hon blev avskedad från sitt kontorsjobb och drunknade i skulder hos Kronofogden. Hon är numera hemlös och försöker överleva på Malmös gator. <br><br>Pia pratar sällan om sitt förflutna, men hon bär det med sig hela tiden. Bilderna på barnen har hon kvar i en sliten plastficka i jackfickan. "Jag vet att jag gjorde misstag", säger hon, "Men jag är fortfarande en människa och jag är fortfarande deras mamma."<br><br>Trots allt håller hon fast vid en gnista hopp. Hon har börjat gå till ett stödcenter i Sankt Knut några gånger i veckan, där hon får varm mat, kläder och möjlighet att prata med en kurator. "Det är inte mycket, men det är något. Och ibland är något tillräckligt för att orka en dag till."'
   },
   {
     missionId: 4,
-    notis1: "Droger funna på skola i Malmö",
-    notis2: "Polisen hatar droger!",
+    notis1: "Larm om droger på skola i Malmö - polisinsats tillkallas omedelbart",
+    notis2: "Poliser larmar: Arbetet mot narkotika skapar oro, sömnsvårigheter och utmattning",
     imgSource: "./media/pictures/kokainjakten.webp",
-    text: "<b>Under 2024 rapporterade polisen i Malmö 27 % fler narkotikarelaterade ärenden än året innan</b><br><br>Mycket av det sker inte i mörka gränder, utan på skolor, innergårdar och ungdomars telefoner. En undersökning visar att nästan var femte Malmöbo upplever att narkotikapåverkade personer är ett problem i deras närmiljö<br><br>Polisen har gjort ett omfattande arbete kring dagens kryptiska chattar. Detta för att lättare stoppa gängkriminalitet och illegal narkotikaförsäljning. Idag har de som säljer narkotika hittat nya sätt att kommunicera med sina köpare. Polisen har upptäckt att det börjat ske en fysisk kommunikation i form av gömda handskrivna lappar."
+    text: "<b>Under 2024 rapporterade polisen i Malmö 27 % fler narkotikarelaterade ärenden än året innan</b><br><br>Mycket av det sker inte i mörka gränder, utan på skolor, innergårdar och ungdomars telefoner. En undersökning visar att nästan var femte Malmöbo upplever att narkotikapåverkade personer är ett problem i deras närmiljö.<br><br>Polisen har gjort ett omfattande arbete kring dagens kryptiska chattar. Detta för att lättare stoppa gängkriminalitet och illegal narkotikaförsäljning. Idag har de som säljer narkotika hittat nya sätt att kommunicera med sina köpare. Polisen har upptäckt att det börjat ske en fysisk kommunikation i form av gömda handskrivna lappar."
   },
 ];
 
@@ -207,7 +207,7 @@ const ending = [
   {
     goodHeader: "VÄL UTFÖRT ARBETE!",
     goodTxt: "Vi har utvärderat er insats under aspiranttjänstsgöringen. Er goda prestation har lett till ett godkänt resultat. Ni har uppfyllt kraven och visat det som krävs för att bli godkända.<br><br>Ni är välkomna att ladda ner ert välförtjänta certifikat. Bra jobbat!",
-    badHeader: "EJ GODKÄNT.",
-    badTxt: "Vi har utvärderat er insats under aspiranttjänstsgöringen. Er prestation har INTE lett till ett godkänt resultat. Ni har INTE uppfyllt kraven eller visat det som krävs för att bli godkända. <br><br>Bättre lycka nästa gång. Ni är välkomna att delta i Insats Blå igen, förutsatt att ni är redo att höja er insats och visa att ni har det som krävs."
+    badHeader: "EJ GODKÄNT!",
+    badTxt: "Vi har utvärderat er insats under aspiranttjänstsgöringen. Er prestation har INTE lett till ett godkänt resultat. Ni har INTE uppfyllt kraven eller visat det som krävs för att bli godkända.<br><br>Bättre lycka nästa gång! Ni är välkomna att delta i Insats Blå igen, förutsatt att ni är redo att höja er insats och visa att ni har det som krävs."
   }
 ];
